b95a34cbe66467c0c111cf3904f9e500f86f69a9
Stefan Behnel <stb@skoobe.de>
2014-10-06 22:07:53 +0200

    mark as stable

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     package_dir={'': 'src'},
 
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
