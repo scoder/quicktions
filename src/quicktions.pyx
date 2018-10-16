f8b7151e82b4dd0429e30b183a9cc92003428f45
Stefan Behnel <stefan_ml@behnel.de>
2018-10-16 21:52:26 +0200

    Prepare release of 1.7.

--- src/quicktions.pyx
+++ src/quicktions.pyx
@@ -21,7 +21,7 @@ from __future__ import division, absolute_import, print_function
 
 __all__ = ['Fraction']
 
-__version__ = '1.6'
+__version__ = '1.7'
 
 cimport cython
 from cpython.unicode cimport Py_UNICODE_TODECIMAL
