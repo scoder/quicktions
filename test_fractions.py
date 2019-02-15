0536e1e8bb6eab0b63e8ce0d406d1e2370f6c783
nocarryr <matt@nomadic-recording.com>
2019-02-15 13:13:09 -0600

    Place Cython-related tests into separate module

--- test_fractions.py
+++ test_fractions.py
@@ -797,50 +797,11 @@ class FractionTest(unittest.TestCase):
             self.assertEqual(ff.numerator, qf.numerator)
             self.assertEqual(ff.denominator, qf.denominator)
 
-class CImportTest(unittest.TestCase):
-
-    def setUp(self):
-        self.build_test_module()
-
-    def tearDown(self):
-        self.remove_test_module()
-
-    def build_test_module(self):
-        self.module_code = '\n'.join([
-            '# cython: language_level=3str',
-            'from quicktions cimport Fraction',
-            'def get_fraction():',
-            '    return Fraction(1, 2)',
-        ])
-        self.base_path = os.path.abspath(os.path.dirname(__file__))
-        self.module_name = 'quicktions_importtest'
-        self.module_filename = os.path.join(self.base_path, '.'.join([self.module_name, 'pyx']))
-        with open(self.module_filename, 'w') as f:
-            f.write(self.module_code)
-
-    def remove_test_module(self):
-        for fn in os.listdir(self.base_path):
-            if not fn.startswith(self.module_name):
-                continue
-            os.remove(os.path.join(self.base_path, fn))
-
-    def test_cimport(self):
-        self.build_test_module()
-        import pyximport
-        self.py_importer, self.pyx_importer = pyximport.install(inplace=True, language_level=3)
-
-        from quicktions_importtest import get_fraction
-
-        self.assertEqual(get_fraction(), F(1,2))
-
-        pyximport.uninstall(self.py_importer, self.pyx_importer)
-
 
 def test_main():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(GcdTest))
     suite.addTest(unittest.makeSuite(FractionTest))
-    suite.addTest(unittest.makeSuite(CImportTest))
     import doctest
     suite.addTest(doctest.DocTestSuite('quicktions'))
     return suite
