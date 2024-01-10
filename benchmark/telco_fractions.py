a9853020be4264efa1fcf6eba978e0ad36cb7b75
Stefan Behnel <stefan_ml@behnel.de>
2024-01-10 10:26:54 +0100

    Improve benchmark results and output.

--- benchmark/telco_fractions.py
+++ benchmark/telco_fractions.py
@@ -42,6 +42,7 @@ The total price, t, is converted to a string, s.
 """
 
 import os
+from math import fsum
 from struct import unpack
 from time import time
 
@@ -101,7 +102,8 @@ def run(cls):
 
 
 def main(n, cls=Fraction):
-    run(cls)  # warmup
+    for _ in range(5):
+        run(cls)  # warmup
     times = []
     for _ in range(n):
         times.append(run(cls))
@@ -113,7 +115,7 @@ if __name__ == "__main__":
     parser = optparse.OptionParser(
         usage="%prog [options]",
         description="Test the performance of the Telco fractions benchmark")
-    parser.add_option("-n", "--num_runs", action="store", type="int", default=12,
+    parser.add_option("-n", "--num_runs", action="store", type="int", default=16,
                       dest="num_runs", help="Number of times to repeat the benchmark.")
     parser.add_option("--use-decimal", action="store_true", default=False,
                       dest="use_decimal", help="Run benchmark with Decimal instead of Fraction.")
@@ -130,3 +132,8 @@ if __name__ == "__main__":
     results = main(options.num_runs, num_class)
     for result in results:
         print(result)
+
+    print()
+    results.sort()
+    print('%.4f  (mean)' % (fsum(results[1:-1]) / (len(results) - 2)))
+    print('%.4f  (median)' % (results[len(results) // 2]))
