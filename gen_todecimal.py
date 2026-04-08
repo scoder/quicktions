07ded41df794e45522e6ca8869a7e8ff39cbd70e
scoder <stefan_ml@behnel.de>
2026-04-08 12:06:14 +0200

    Support abi3 wheel build and use it for less common targets (GH-37)
    * Support building against the Limited API by providing fallback implementations.
    
    * Avoid raising costly exceptions for large numbers in the GCD Python fallback code.
    Integer comparisons still tend to be faster than creating a new exception, raising and catching it.
    
    * Use math.gcd() in Limited API on Python 3.13+.
    
    * Build: Serve many lesser used platforms with abi3 wheels.
    
    * Work around Cython forgetting to include "stdlib.h".

---
+++ gen_todecimal.py
@@ -0,0 +1,57 @@
+"""
+Generate and print a C snippet that maps Unicode digit code points to ASCII digits.
+"""
+
+import sys
+import unicodedata
+from collections import defaultdict
+
+
+def list_digits():
+    category_of = unicodedata.category
+    return [
+        ch for ch in range(128, 1114111+1)
+        if category_of(chr(ch)) == 'Nd'
+    ]
+
+
+def map_to_ascii_digit(digits):
+    adigit_to_udigit = defaultdict(list)
+    for ch in digits:
+        adigit = int(chr(ch))
+        if ch & 15 == adigit:
+            adigit_to_udigit['{digit} & 15'].append(ch)
+        elif (ch - 6) & 15 == adigit:
+            adigit_to_udigit['({digit} - 6) & 15'].append(ch)
+        elif (ch - 0x116da) & 15 == adigit:
+            adigit_to_udigit['({digit} - 0x116da) & 15'].append(ch)
+        elif (ch - 0x1e5f1) & 15 == adigit:
+            adigit_to_udigit['({digit} - 0x1e5f1) & 15'].append(ch)
+        elif ch >= 0x1d7ce and (ch - 0x1d7ce) % 10 == adigit:
+            adigit_to_udigit['({digit} - 0x1d7ce) % 10'].append(ch)
+        else:
+            adigit_to_udigit[str(adigit)].append(ch)
+    return adigit_to_udigit
+
+
+def gen_switch_cases(digits_by_adigit):
+    pyver = sys.version_info
+    print(f"/* Switch cases generated from Python {pyver[0]}.{pyver[1]}.{pyver[2]} {pyver[3]} {pyver[4]} */")
+    print("/* Deliberately excluding ASCII digit characters. */")
+    print()
+    print(f"/* Lowest digit: {min(ch for characters in digits_by_adigit.values() for ch in characters)} */")
+    print("switch (digit) {")
+
+    for adigit_format, udigits in digits_by_adigit.items():
+        for ch in udigits:
+            print(f'    case 0x{ch:x}:')
+
+        print(f"        return {adigit_format.format(digit='digit')};")
+
+    print("    default:")
+    print("        return -1;")
+    print("}")
+
+
+if __name__ == '__main__':
+    gen_switch_cases(map_to_ascii_digit(list_digits()))
